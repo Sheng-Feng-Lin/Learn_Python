e883e3445b24a9703dfb021a47d3aa6dbb4c9c89
LouisLin <a9310139@gmail.oom>
2015-08-19 22:21:29 +0800

    reverse triangle

--- triangle.py
+++ triangle.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-i = 0 
-j = 0
-
-for i in xrange(4):
-    for j in xrange(4-i-1):
+for i in xrange(4, 0, -1): # for loop 4, 3, 2, 1.
+    for j in xrange(4-i):
         print ' ',
-    for j in xrange(i*2+1):
+    for j in xrange(i*2-1):
         print '*',
     print 
