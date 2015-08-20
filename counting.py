cd509a3baedfa89e874bf51e716fa6901cd01a1d
LouisLin <LouisLin@sanjetco.com>
2015-08-20 19:39:05 +0800

    use directory to count characters

---
+++ counting.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+s = 'Please count the characters here.'
+d = {}
+
+for c in s:
+    if c in d:
+        d[c] += 1
+    else:
+        d[c] = 1
+
+print d 
